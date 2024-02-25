557f90b1e9073d12191f0f88706db76b2894a1e2
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-02-25 20:44:08 -0300

    commit inicial

---
+++ Projetos/Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Calculator
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) {
+                Console.Clear();
+                Console.WriteLine("Primeiro valor: ");
+                float v1 = float.Parse(Console.ReadLine());
+                Console.WriteLine("Segundo valor: ");
+                float v2 = float.Parse(Console.ReadLine());
+                Console.WriteLine("Operação que deseja realizar:");
+                Console.WriteLine("+ | - | x | /");
+                string operacaoDesejada = Console.ReadLine();
+
+                switch (operacaoDesejada) {
+                    case "+": Console.WriteLine($"{v1} + {v2} = {(v1 + v2)}"); break;
+                    case "-": Console.WriteLine($"{v1} - {v2} = {(v1 - v2)}"); break;
+                    case "x": Console.WriteLine($"{v1} x {v2} = {(v1 * v2)}"); break;
+                    case "/": Console.WriteLine($"{v1} / {v2} = {(v1 / v2)}"); break;
+                    default: Console.WriteLine("ERRO: Operação inválida"); break;
+                }
+
+                Console.WriteLine("Deseja continuar ? (s/n)");
+                string continuar = Console.ReadLine();
+
+                if (continuar == "n"){
+                    Console.WriteLine("Obrigado por utilizar a calculadora !");
+                    break;
+                }
+            };
+        }
+    }
+}
---
+++ Teste/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Teste
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            var mouse = new Product(002, "Logitech", 110.00);
+            Console.WriteLine(mouse.Id);
+            Console.WriteLine(mouse.Name);
+            Console.WriteLine(mouse.Price);
+            Console.WriteLine(mouse.PriceInDolar(5));
+        }
+    }
+
+    struct Product 
+    {
+        public Product(int id, string name, double price) //metodo construtor, não retorna nada
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public int Id;
+        public string Name;
+        public double Price;
+
+        public double PriceInDolar(double dolar) { //metodo
+            return Price * dolar;
+        }
+    }
+}
