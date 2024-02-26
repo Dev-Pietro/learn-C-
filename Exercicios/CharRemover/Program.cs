8262e51c8e9916392205e1ebefa1f6ca9a3477d5
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-02-25 21:51:18 -0300

    feito um exercicio para praticar lógica

---
+++ Exercicios/CharRemover/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CharRemover
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a word:");
+            string userWord = Console.ReadLine();
+            Console.WriteLine("Which character you want to remove ?"); 
+            string charWant = Console.ReadLine();
+            string wordWithCharRemove = "";
+            
+            for (int count = 0; count < userWord.Length; count++)
+            {
+                if (userWord[count].ToString() == charWant) {
+                    continue;
+                }
+                wordWithCharRemove += userWord[count].ToString();
+            }
+
+            Console.WriteLine($"The word with the specified character removed: {wordWithCharRemove}");
+        }
+    }
+}
+
+
