8108417d2bc541ba5f3c10c3234daf2f598e2993
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-02-27 18:54:18 -0300

    TextEditor (Editor de Texto Simples)

--- Projetos/TextEditor/Program.cs
+++ Projetos/TextEditor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace TextEditor
 {
@@ -28,12 +29,52 @@ namespace TextEditor
 
         static void Open()
         {
+            Console.Clear();
+            Console.WriteLine("What path of file ?");
+            string path = Console.ReadLine();
 
+            using(var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
         }
 
         static void Edit()
         {
+            Console.Clear();
+            Console.WriteLine("Type your text bellow (ESC to exit)");
+            Console.WriteLine("----------------------");
+            string text = "";
 
+            do 
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine; //colocando nova linha a cada fim de escrita
+            }
+            while(Console.ReadKey().Key != ConsoleKey.Escape); //enquanto usuario não apertar ESC não irá sair
+
+            Save(text);
+        }
+    
+        static void Save(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("What path to save this file ?");
+            var path = Console.ReadLine();
+
+            using (var file =  new StreamWriter(path)) //abrindo e fechando o arquivo através do caminho e com StreamWriter
+            {   
+                file.Write(text);
+            }
+
+            Console.WriteLine($"File {path} saved successfully !");
+            Console.ReadLine();
+            Menu();
         }
     }
 }
