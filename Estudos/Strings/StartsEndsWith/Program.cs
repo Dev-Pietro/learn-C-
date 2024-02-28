b7e44c9f90a99941e1418e77ab96a2246f131041
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-02-28 19:41:35 -0300

    StartsWith EndsWith

---
+++ Estudos/Strings/StartsEndsWith/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StartsEndsWith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Isto é um texto";
+            Console.WriteLine(texto.StartsWith("Isto")); //retorna booleano
+            Console.WriteLine(texto.StartsWith("Esse")); 
+
+            Console.WriteLine(texto.EndsWith("texto")); //retorna booleano
+            Console.WriteLine(texto.EndsWith("batata"));
+        }
+    }
+}
