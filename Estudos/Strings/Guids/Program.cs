4191c0677d2bb087c5e2097304ec1952933ffd74
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-02-28 18:58:09 -0300

    Guids - Básico

---
+++ Estudos/Strings/Guids/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Guids
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            var id = Guid.NewGuid(); //gera um id novo
+            id.ToString();
+            Console.WriteLine(id);
+        }
+    }
+}
