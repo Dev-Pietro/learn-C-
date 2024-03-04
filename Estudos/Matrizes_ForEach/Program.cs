1b1fa4702c6e04c3e185494ed3838e77a9c6b3b9
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-03-04 20:07:15 -0300

    Estudando Matriz e ForEach

--- Estudos/Matrizes_ForEach/Program.cs
+++ Estudos/Matrizes_ForEach/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Matrizes_ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string[] fraudlentOrderIDs = new string[3];
+
+            fraudlentOrderIDs[0] = "A123";
+            fraudlentOrderIDs[1] = "B456";
+            fraudlentOrderIDs[2] = "C789";
+            */
+            string[] suspects = {"Batman", "Robin", "Joker"}; 
+            foreach (string name in suspects)
+            {
+                Console.WriteLine(name);
+            };
+
+            int[] inventory = {200, 100, 50};
+            int totalPrice = 0;
+            int bin = 0;
+            foreach (int price in inventory)
+            {
+                totalPrice += price;
+                bin++;
+                Console.WriteLine($"Bin {bin}: {price} (Total: {totalPrice})");
+            };
+
+            Console.WriteLine($"Inventory Price: {totalPrice}.");
+
+        }
+    }
+}
