23ed20684eb3cf7bf1af8957f570963c007a4975
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-03-04 20:34:36 -0300

    Exericicio para treinar conhecimentos básicos de Matrizes

---
+++ Exercicios/FraudlentOrders/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FraudlentOrders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //orders that starts with B have a greater chance to be a fraudlent
+            string[] orders = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+            int index = 0;
+            foreach (string order in orders)
+            {
+                index++;
+                if (order.StartsWith("B")) {
+                    Console.WriteLine($"Fraudlent order founded: {order} (Order {index})");
+                }
+            }
+        }
+    }
+}
