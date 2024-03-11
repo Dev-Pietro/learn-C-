9cc14107245baebb6d78924b0fc201e23c5124d1
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-03-11 18:21:10 -0300

    Ex apenas para revisar e por causa de uma discussao no discord

---
+++ Exercicios/NumeroPrimo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NumeroPrimo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i < 30; i++) {
+                Console.WriteLine($"{i} é número primo ? {VerificaPrimo(i)}");
+            }
+        }
+
+        static bool VerificaPrimo(int num)
+        {
+            if (num == 2 || num == 3)
+                return true;
+            
+            if (num % 2 == 0 || num % 3 == 0)
+                return false;
+            
+            return true;
+        }
+    }
+}
