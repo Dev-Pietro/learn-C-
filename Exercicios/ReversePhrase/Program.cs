2106583c62d3bcac602af4e985db525df0bb188d
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-03-13 19:44:38 -0300

    Inverte a frase do usuario - Para treinar alguns metodos básicos

---
+++ Exercicios/ReversePhrase/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReversePhrase
+{
+    class Programming
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a Phrase:");
+            var phrase = Convert.ToString(Console.ReadLine());
+
+            char[] phraseArray = phrase.ToCharArray();
+            Array.Reverse(phraseArray);
+
+            string phraseReverse = new string(phraseArray);
+
+            Console.WriteLine($"Reverse Phrase: {phraseReverse}");
+        }
+    }
+}
