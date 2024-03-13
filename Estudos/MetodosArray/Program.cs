ff10dab229444f27f858aa1ea250d19c1ac95765
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-03-13 19:00:55 -0300

    Métodos Básicos em Array

---
+++ Estudos/MetodosArray/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MetodosArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] codes = {"B12", "B14", "C01", "A22", "A10", "Z05"};
+
+            Array.Sort(codes); //sort
+            foreach(string code in codes) {
+                Console.WriteLine($"Code: {code}");
+            }
+
+            Console.WriteLine("-----------------------------------------------------");
+
+            Array.Reverse(codes); //reverse
+            foreach(string code in codes) {
+                Console.WriteLine($"Code: {code}");
+            }
+
+            Console.WriteLine("-----------------------------------------------------");
+
+            Array.Clear(codes, 0, 2); //remove
+            foreach(string code in codes) {
+                Console.WriteLine($"Code: {code}");
+            }
+
+            Console.WriteLine("-----------------------------------------------------");
+
+            Array.Resize(ref codes, 8); //"add" or "remove"
+
+            codes[6] = "B02";
+            codes[7] = "F05";
+
+            foreach(string code in codes) {
+                Console.WriteLine($"Code: {code}");
+            }
+        }
+    }
+}
