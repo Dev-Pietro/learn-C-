c3432b1e7cfb58c87f060fd2311ed09b6e055092
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-03-13 20:00:40 -0300

    Exercicio para praticar transformação de string em array

---
+++ Exercicios/CheckIDs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CheckIDs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ids = "B123,C234,A345,C15,B177,G3003,C235,B179";
+            string[] arrayIds = ids.Split(',');
+            Array.Sort(arrayIds);
+            foreach (string id in arrayIds) {
+                if(id.Length == 4) {
+                    Console.WriteLine(id);
+                    continue;
+                }
+                Console.WriteLine($"{id}\t- Error");
+            }
+        }
+    }
+}
