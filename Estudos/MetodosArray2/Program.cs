51953b58b206b47187b1757f75873deab8017ac9
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-03-13 19:20:37 -0300

    Outros métodos básicos de array

---
+++ Estudos/MetodosArray2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MetodosArray2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "abcde12345";
+            char[] valueArray = value.ToCharArray(); //Convert string to char array
+
+            foreach(char x in valueArray){
+                Console.WriteLine($"Value - {x}");
+            }
+
+            string result = String.Join("-", valueArray); //Convert array to separate string with "-"
+            Console.WriteLine(result);
+
+            string[] items = result.Split("-"); //Separate the array with the char that you want
+            foreach(string i in items) {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
