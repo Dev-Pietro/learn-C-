0d102866a26c2bc41e7fc002ab926322c0c42626
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-03-17 14:44:17 -0300

    Exercicio só para treinar iteração em matrizes

---
+++ Exercicios/MaiorValor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace MaiorValor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {3, 5, 20, 10, 2};
+            var h_val = nums[0];
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > h_val)
+                    h_val = nums[i]; 
+            }
+
+            Console.WriteLine(h_val);
+        }
+    }
+}
