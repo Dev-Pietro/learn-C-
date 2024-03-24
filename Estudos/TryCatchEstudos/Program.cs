02abac14df24640c2474f313aeddb5483d349708
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-03-23 23:05:43 -0300

    Estudos sobre Try-Catch, básico

---
+++ Estudos/TryCatchEstudos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TryCatchEstudos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "ab";
+            int y = 0;
+
+            try
+            {
+                int xInt = Convert.ToInt32(x);
+                Console.WriteLine($"{x} + {y} = {x + y}");
+                // Console.WriteLine($"{x} / {y} = {x / y}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+}
