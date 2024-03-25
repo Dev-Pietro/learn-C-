6d0c1fcf21b04df0eed784a133f3b2f65af3bcaa
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-03-25 19:39:08 -0300

    Exercicio básico - Area Triangulo

---
+++ Exercicios/AreaTriangulo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace AreaTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double xA, xB, xC, yA, yB, yC;
+
+            Console.WriteLine("Digite as medidas do triangulo X:");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite as medidas do triangulo Y:");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (xA + xB + xC) / 2.0;
+            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+            p = (yA + yB + yC) / 2.0;
+            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+            Console.WriteLine($"Area de X: {areaX:N4} \nArea de Y: {areaY:N4}");
+
+            if (areaX > areaY) {
+                Console.WriteLine("Area de X é a maior area.");
+            } else {
+                Console.WriteLine("Area de Y é a maior area.");
+            } 
+
+        }
+    }
+}
