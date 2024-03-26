4abe5bc0ba15cf190a7282e2fa616c6cf2db875f
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-03-26 20:11:03 -0300

    OOP Aula#2 - Colocando método área na classe Triangulo

---
+++ Estudos/OOP/Triangulo_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace Triangulo_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Digite as medidas do triangulo X:");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite as medidas do triangulo Y:");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = x.Area();
+            double areaY = y.Area();
+
+            Console.WriteLine($"Area de X: {areaX:N4} \nArea de Y: {areaY:N4}");
+
+            if (areaX > areaY) {
+                Console.WriteLine("Area de X é a maior area.");
+            } else {
+                Console.WriteLine("Area de Y é a maior area.");
+            } 
+        }
+    }
+}
