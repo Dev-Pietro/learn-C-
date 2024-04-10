221dfd56857999bc1eb3298ba9b2804c709bbcf0
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-04-09 21:00:58 -0300

    OOP - Ex06

---
+++ Exercicios/OOP_Exercicios/Ex06/Aluguel.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Ex06
+{
+    class Aluguel
+    {
+        public string Nome;
+        public string Email;
+
+        public Aluguel(string nome, string email) {
+            Nome = nome;
+            Email = email;
+        }
+    }
+}
---
+++ Exercicios/OOP_Exercicios/Ex06/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ex06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluguel[] aluguel = new Aluguel[10];
+            Console.WriteLine("Quantos quartos serão alugados ?");
+            int quartosAlug = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            int count = 0;
+
+            for(int i = 0; i < quartosAlug; i++) {
+                count++;
+                Console.WriteLine($"Aluguel #{count}\nNome:");
+                string nome = Console.ReadLine();
+                Console.WriteLine("Email:");
+                string email = Console.ReadLine();
+                Console.WriteLine("Quarto:");
+                int numQuarto = int.Parse(Console.ReadLine());
+
+                aluguel[numQuarto] = new Aluguel(nome, email);
+                Console.WriteLine("");
+            };
+
+            Console.WriteLine("Quartos Ocupados:");
+
+            for(int i = 0; i < aluguel.Length; i++) {
+                if(aluguel[i] == null) continue;
+                else {
+                    Console.WriteLine($"{i}: {aluguel[i].Nome}, {aluguel[i].Email}");
+                }
+            };
+        }
+    }
+}
