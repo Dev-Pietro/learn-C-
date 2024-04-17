e5b5e0d2f8054ddfae5fda4b42f4942759f49e2e
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-04-17 17:04:51 -0300

    OOP Aula#13 - Dando uma recaptulada em matrizes, dar uma revisada no futuro

---
+++ Estudos/OOP/Matriz_13/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Matriz_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double[,] mat = new double[2, 3]; //matriz bidimensional - 2 linhas e 3 colunas
+
+           Console.WriteLine(mat.Length); //quantos elementos tem no total
+
+           Console.WriteLine(mat.Rank); //quantas linhas tem no total
+        }
+    }
+}
