900da471ad86c88fb0781266b6c2f0ce30e9742c
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-04-19 16:39:01 -0300

    OOP - Ex09 OBS: Outro exercicio bom de se revisar

---
+++ Exercicios/OOP_Exercicios/Ex09/Entities/Comment.cs
@@ -0,0 +1,16 @@
+using System; 
+
+namespace Ex09.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment(){}
+
+        public Comment(string text) {
+            Text = text;
+        }
+
+    }
+}
