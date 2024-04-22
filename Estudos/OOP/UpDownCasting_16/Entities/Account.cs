115f0d93c344a27720e2e169130c0b9af1c04795
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-04-22 18:16:20 -0300

    OOP Aula#16 - Dando uma olhada em upcasting e downcasting, revisar no futuro

---
+++ Estudos/OOP/UpDownCasting_16/Entities/Account.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace UpDownCasting_16.Entities
+{
+    class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; } //só pode ser alterado através dessa classe ou de uma subclasse (businessaccount)
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ Estudos/OOP/UpDownCasting_16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UpDownCasting_16.Entities;
+
+namespace UpDownCasting_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account(1001, "Alex", 0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0, 500);
+
+            //UPCASTING
+
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0, 200);
+            Account acc3 = new SavingsAccount(1004, "Ana", 0, 0.01);
+
+            //DOWNCASTING
+
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100);
+
+        }
+    }
+}
