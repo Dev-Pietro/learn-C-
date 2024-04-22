9dda20429c160ba4f46e847169a06b3bf8bde22b
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-04-22 17:37:50 -0300

    OOP Aula#15 - Estudando sobre Herança e dando uma olhada no atributo protected de set

---
+++ Estudos/OOP/Heranca2_15/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Heranca2_15.Entities;
+
+namespace Heranca2_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob", 100, 500);
+
+            account.Balance = 10;
+
+            Console.WriteLine(account.Balance); //forçando o erro para mostrar o uso de protected;
+                                                //que deixa o atributo da superclass só poder ser alterado de classes que derivam dele, ou subclasses 
+        }
+    }
+}
