882a435cebf7a570ed8a9de98db91c0f08414372
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-04-26 20:42:50 -0300

    OOP - Ex14 OBS: Exercicio sobre Abstract

---
+++ Exercicios/OOP_Exercicios/Ex14/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Ex14.Entities;
+
+namespace Ex14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<TaxPayer> list = new List<TaxPayer>();
+
+            Console.WriteLine("Enter the number of tax payers: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Tax payer #{i} data");
+                Console.WriteLine("Individual or company (i/c)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.WriteLine("Name: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Anual income: ");
+                double anualIncome = double.Parse(Console.ReadLine());
+
+                if (ch == 'i')
+                {
+                    Console.WriteLine("Health expenditures: ");
+                    double healthExpenditures = double.Parse(Console.ReadLine());
+                    list.Add(new Individual(name, anualIncome, healthExpenditures));
+                } else
+                {
+                    Console.WriteLine("Number of employees: ");
+                    int numberOfEmployees = int.Parse(Console.ReadLine());
+                    list.Add(new Company(name, anualIncome, numberOfEmployees));
+                }
+            }
+
+            Console.WriteLine("TAXES PAID");
+            double sum = 0;
+            foreach (TaxPayer payer in list)
+            {
+                double tp = payer.Tax();
+                Console.WriteLine($"{payer.Name} ${tp:N2}");
+                sum += tp;
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine($"TOTAL TAXES: ${sum:N2}");
+        }
+    }
+}
