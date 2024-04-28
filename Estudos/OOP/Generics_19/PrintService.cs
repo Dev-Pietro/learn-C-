0852bb832c56a875016e00a056a1d8a5216318be
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-04-28 13:14:49 -0300

    OOP Aula#19 - Aprendendo o que são Generics e como/quando utiliza-los

---
+++ Estudos/OOP/Generics_19/PrintService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Generics_19
+{
+//Generics - permite o reuso da classe com diversos tipos, além de uma melhor performance, no exemplo abaixo o tipo
+//<T> representa o tipo que a pessoa irá escolher para realizar a função, podendo ser desde int até string
+    class PrintService<T>
+    {
+        private T[] _values = new T[10];
+        private int _count = 0;
+
+        public void AddValue(T value)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("PrintService is full");
+            }
+
+            _values[_count] = value;
+            _count++;
+        }
+
+        public T First()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("PrintService is empty");
+            }
+
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_values[i] + ", ");
+            }
+            if (_count > 0)
+            {
+                Console.Write(_values[_count - 1]);
+            }
+
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ Estudos/OOP/Generics_19/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Generics_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<string> printservice = new PrintService<string>(); //utilizando Generics
+
+            Console.WriteLine("How many values ?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string x = Console.ReadLine();
+                printservice.AddValue(x);
+            }
+
+            printservice.Print();
+            Console.WriteLine("First: " + printservice.First());
+        }
+    }
+}
