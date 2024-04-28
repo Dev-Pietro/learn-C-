71a9007f60c128bfbb329ed39bc6bb17cd796ea3
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-04-28 14:28:43 -0300

    OOP Aula#20 - Dando uma olhada em Equals e GetHashCode e suas diferenças de uso

---
+++ Estudos/OOP/GetHashCodeEquals_20/Entities/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GetHashCodeEquals_20.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public override bool Equals(object? obj) //criando um equals personalizado para Client utilizando o Email como referência
+        {
+            if (!(obj is Client))
+            {
+                return false;
+            }
+
+            Client other = obj as Client;
+            return Email.Equals(other.Email);    
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode(); //criando um GetHashCode personalizado para Client utilizando Email como referência
+        }
+    }
+}
---
+++ Estudos/OOP/GetHashCodeEquals_20/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using GetHashCodeEquals_20.Entities;
+
+namespace GetHashCodeEquals_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "alex@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+            //Equals - lento, porém preciso
+            //GetHashCode - rápido, porém com chance de colisão
+            //No exemplo acima foi criado um equals e um gethashcode personalizado para a classe Client
+        }
+    }
+}
