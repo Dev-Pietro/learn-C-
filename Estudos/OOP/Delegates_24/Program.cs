5d9cea66a6e7c77cbf3bc897c97cf1fac3ad0fd4
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-05-02 20:43:18 -0300

    OOP Aula#24 - Introdução a delegates no geral e oque são

---
+++ Estudos/OOP/Delegates_24/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Delegates_24.Services;
+
+namespace Delegates_24
+{
+    //delegate - tipo referência, que no exemplo referencia a uma função, com type safety
+    delegate double BinaryNumericOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationServices.Sum;
+
+            double Result = op(a, b);
+            Console.WriteLine(Result);
+        }
+    }
+}
