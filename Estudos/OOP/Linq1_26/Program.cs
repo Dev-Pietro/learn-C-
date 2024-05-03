3770ff1b84b45bfb407e3da637f8abad40a0cf6f
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-05-03 16:22:32 -0300

    OOP Aula#26 - Introdução a biblioteca LINQ

---
+++ Estudos/OOP/Linq1_26/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq1_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Specify the Data Source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            //Define the query expression
+            var result = numbers.Where(x => x % 2 == 0).Select(x => x * 10); 
+
+            //Execute the query expression
+            foreach (int x in result)
+            {
+                Console.WriteLine(x); //20 - 40
+            }
+        }
+    }
+}
