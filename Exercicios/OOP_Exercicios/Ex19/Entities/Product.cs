f39a5931ae38fb8b02dc0ba9b20236b9964e3a9e
Gabriel Pietro <gabriel10.pietro@gmail.com>
2024-05-04 19:08:58 -0300

    OOP - Ex19 OBS: exercicio usando linq

---
+++ Exercicios/OOP_Exercicios/Ex19/Entities/Product.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Ex19.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price){
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ Exercicios/OOP_Exercicios/Ex19/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using Ex19.Entities;
+using System.Globalization;
+
+namespace Ex19
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();  
+
+            List<Product> list = new List<Product>();
+
+            using (StreamReader sr = File.OpenText(path)) {
+                while (!sr.EndOfStream) {
+                    string[] fields = sr.ReadLine().Split(',');
+                    string name = fields[0];
+                    double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                    list.Add(new Product(name, price));
+                }
+            }
+
+            var avg = list.Select(p => p.Price).DefaultIfEmpty(0).Average();
+            Console.WriteLine($"Average price: {avg:N2} ");
+
+            var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+            foreach(string name in names) {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
